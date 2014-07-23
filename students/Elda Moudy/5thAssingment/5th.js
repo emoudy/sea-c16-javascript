e96fae41b4fa998e62749e7ec3042abec6f3d8bd
Elda S Moudy <elda@moudy.net>
2014-07-23 15:13:00 -0700

    Adding 5th Assingment

---
+++ students/Elda Moudy/5thAssingment/5th.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+	
+  //make tabs: 
+	$(function() {
+    $('#top_tabs').tabs();
+  });
+
+// --------------------------
+// This is work in progress...I'm not sure why it's not working.
+// ---------------------------
+
+	// add task to list  
+
+	// $('#submit_task').click(function() {
+	// 	var addNewTask = $('#newTaskDescription').val();
+ //  	if (addNewTask) {
+ //      $('#taskList').append('<div class="currentTask"><input type="checkbox"/>' + addNewTask + '</div>');
+	// 		$('#newTaskDescription').val('');
+	// 	};
+	// });
+
+	// remove task from list
+	
+	// $("#delete_task").click(function() {
+	// 	$('.currentTask').has('input:checkbox:checked').remove();
+	// });
+});
+
+
+
+
+
+
+
+
+
+
+//			$('.currentTask').fadeOut(500, function() {
+//			});
+//		$('.currentTask').toggle("puff", 500);
+//	});
